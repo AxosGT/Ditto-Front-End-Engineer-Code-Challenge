e956d1f5455cab266c8d8b3ae43dff625c2db42b
AxosGT <chaosxaos@gmail.com>
2017-07-07 14:07:25 -0700

    Boilerplate
    Boilerplate and some CSS Boilerplate

--- ditto-paige-app/src/App.js
+++ ditto-paige-app/src/App.js
@@ -1,20 +1,26 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //this.props.onPageLode();
+  };
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    switch (this.props.activePage){
+      case 'home':
+        //return <ContainerHome />
+      case 'rePage':
+        //return <ContainerRePage />
+      default:
+        return(
+          <h1>page failed to become active</h1>
+        );
+    }
+
   }
 }
 
